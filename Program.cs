9391a5b0286c1f7becc25cf794ae9ef34f022b56
Artem <burkun_artem@mail.ru>
2022-12-22 14:28:51 +0300

    add progs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"hello", "21", "world", ":-)", "123", "124rsdfv"};
+int lengthNewArray = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        lengthNewArray++;
+    }
+}
+
+string[] newArray = new string[lengthNewArray];
+
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[j] = array[i] ;
+        j++;
+    }
+}
+
+void PrintArray (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.WriteLine();
+}
+
+PrintArray(newArray);
